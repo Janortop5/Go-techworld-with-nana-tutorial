eca5c6f98cb447ec44ff8d1d77795bcadcd96de5
Emmanuel Aladejana <emmanuelaladejana05@gmail.com>
2026-05-26 20:02:15 +0100

    complete tutorial

--- go-devops-techworld-with-nana-2/main.go
+++ go-devops-techworld-with-nana-2/main.go
@@ -1,33 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+var shortGolang =  "Watch Go crash course"
+var FullGolang  = "Watch Nana's Golang Full Course"
+var rewardDessert = "Reward myself with a donut"
 
-//	var shortGolang =  "Watch Go crash course"
-//	var FullGolang  = "Watch Nana's Golang Full Course"
-//	var rewardDessert = "Reward myself with a donut"
-
-	//rewardDessert := "Reward myself with a donut"
+	// rewardDessert := "Reward myself with a donut"
 	// var maxItemsInGroup = 20
 
-//	var taskItems = [3]string {shortGolang, FullGolang, rewardDessert }
+var taskItems = []string {shortGolang, FullGolang, rewardDessert }
 
 func main() {
 
-		fmt.Println("####### Welcome to our Todolist App! #######")
+	//	fmt.Println("####### Welcome to our Todolist App! #######")
 
-		var shortGolang =  "Watch Go crash course"
-		var FullGolang  = "Watch Nana's Golang Full Course"
-		var rewardDessert = "Reward myself with a donut"
-	// rewardDessert := "Reward myself with a donut"
-  //  var maxItemsInGroup = 10
+   // var shortGolang =  "Watch Go crash course"
+	 //	var FullGolang  = "Watch Nana's Golang Full Course"
+	 //	var rewardDessert = "Reward myself with a donut"
+	//rewardDessert := "Reward myself with a donut"
+  //var maxItemsInGroup = 10
+	 //	var taskItems = []string {shortGolang, FullGolang, rewardDessert }
 
-		var taskItems = []string {shortGolang, FullGolang, rewardDessert }
+	 // printTasks(taskItems)
+	 // fmt.Println()
 
-		printTasks(taskItems)
-		println()
-		addTask(taskItems, "Go for a run")
-		println()
-		printTasks(taskItems)
+	 // taskItems = addTask(taskItems, "Go for a run")
+
+	 // fmt.Println("Updated list")
+		//printTasks(taskItems)
 		// fmt.Println(shortGolang)
 		// fmt.Println(FullGolang)
 		//fmt.Println(rewardDessert)
@@ -46,22 +49,37 @@ func main() {
 		// fmt.Println()
 		// fmt.Println("My Project")
 		// fmt.Println(FullGolang)
+
+		http.HandleFunc("/", helloUser)
+		http.HandleFunc("/show-tasks", showTasks)
+
+		http.ListenAndServe(":8080", nil)
 }
 
-func printTasks(taskItems []string,) {
-		fmt.Println("list of my Todos")
-		// fmt.Println("Tasks:", taskItems)
-		for index, task := range taskItems {
-			  // fmt.Println(index+1, ".", task)
-				// fmt.Printf("%d; %s\n", index+1, task)
-				// fmt.Printf("%d. %s\n", index+1, task)
-        fmt.Printf("%d: %s\n", index+1, task)
-
+func showTasks(writer http.ResponseWriter, request *http.Request) {
+		for _, task := range taskItems {
+				fmt.Fprintln(writer, task)
 		}
 }
 
-func addTask(taskItem []string, newTask string) {
-		var updatedTaskItems = append(taskItem, newTask)
-		printTasks(updatedTaskItems)
-		
+func helloUser(writer http.ResponseWriter, request *http.Request) {
+		var greeting = "Hello user. Welcome to our TodoList App!"
+		fmt.Fprintln(writer, greeting)
 }
+
+//func printTasks(taskItems []string,) {
+//		fmt.Println("list of my Todos")
+		// fmt.Println("Tasks:", taskItems)
+//		for index, task := range taskItems {
+			  // fmt.Println(index+1, ".", task)
+				// fmt.Printf("%d; %s\n", index+1, task)
+//				// fmt.Printf("%d. %s\n", index+1, task)
+//        fmt.Printf("%d: %s\n", index+1, task)
+
+//		}
+// }
+
+// func addTask(taskItem []string, newTask string) []string {
+//		var updatedTaskItems = append(taskItem, newTask)
+//		return updatedTaskItems
+// }
